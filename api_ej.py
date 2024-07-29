8bf3ec8029653c49514fc58675fbc887b7308579
willkattrup <williamekattrup@gmail.com>
2024-07-28 23:58:36 -0400

    updated ej prompt

--- api_ej.py
+++ api_ej.py
@@ -25,24 +25,24 @@ class APIEJClient():
                     {statement}
                     
                     CODEBOOK:
-                    <category id="1">1 - Justice weaponization: The use of justice rhetoric to oppose climate/environmental action.</category>
-                    <category id="1.1">1.1 - Fossil fuels / Deregulation help marginalized people</category>
-                    <category id="1.1.1">1.1.1 - Developing countries need/deserve fossil fuels.</category>
-                    <category id="1.2">1.2 - Pro-environment policies hurt marginalized people: Environmental regulation and climate action hurt marginalized people.</category>
-                    <category id="1.2.1">1.2.1 - Pro-environment policies increase the cost of goods and services, potentially harming marginalized communities.</category>
-                    <category id="1.2.2">1.2.2 - The energy transition will disproportionately hurt the developing world.</category>
-                    <category id="2">2 - Environmental justice as waste: Environmental or climate justice policies are a waste of government resources.</category>
-                    <category id="2.1">2.1 - Environmental justice initiatives add unnecessary bureaucracy</category>
-                    <category id="2.1.1">2.1.1 - Environmental justice initiatives will slow processes down necessary to address the climate emergency: The bureacratic burden created by EJ policies will hamper the development of renewable energy infrastructure.</category>
-                    <category id="3">3 - Environmental justice doubt: Environmental/climate injustice are not real</category>
-                    <category id="3.1">3.1 - Questioning the science: There is not a scientific consensus that confirms the disproportionate impacts of environmental harm on marginalized communities.</category>
-                    <category id="3.2">3.2 - Environment and justice don't mix: Social justice does not belong in environmental policies or movements.</category>
-                    <category id="3.3">3.3 - Marginalized people are to blame for their own problems</category>
-                    <category id="3.4">3.4 - Environmental justice is silly: Environmental justice ridiculous and even laughable, including sarcastic attitude to the EJ movement.</category>
-                    <category id="4">4 - Environmental justice mischaracterization.</category>
-                    <category id="4.1">4.1 - Environmental justice is racist: Environmental/climate justice movements/policies will only sew further division or otherwise lead to further marginalization.</category>
-                    <category id="4.2">4.2 - Environmental justice has no clear definition</category>
-                    <category id="4.3">4.3 - Environmental justice as trojan horse: Environmental/climate justice movements/policies are simply a way for politicians/activists to assert a malicious ulterior motive.</category>
+                    <category id="1">1 - Justice weaponization: The use of justice rhetoric to oppose climate/environmental action.</category id="1">
+                    <category id="1.1">1.1 - Fossil fuels / Deregulation help marginalized people</category id="1.1">
+                    <category id="1.1.1">1.1.1 - Developing countries need/deserve fossil fuels.</category id="1.1.1">
+                    <category id="1.2">1.2 - Pro-environment policies hurt marginalized people: Environmental regulation and climate action hurt marginalized people.</category id="1.2">
+                    <category id="1.2.1">1.2.1 - Pro-environment policies increase the cost of goods and services, potentially harming marginalized communities.</category id="1.2.1">
+                    <category id="1.2.2">1.2.2 - The energy transition will disproportionately hurt the developing world.</category id="1.2.2">
+                    <category id="2">2 - Environmental justice as waste: Environmental or climate justice policies are a waste of government resources.</category id="2">
+                    <category id="2.1">2.1 - Environmental justice initiatives add unnecessary bureaucracy</category id="2.1">
+                    <category id="2.1.1">2.1.1 - Environmental justice initiatives will slow processes down necessary to address the climate emergency: The bureacratic burden created by EJ policies will hamper the development of renewable energy infrastructure.</category id="2.1.1">
+                    <category id="3">3 - Environmental justice doubt: Environmental/climate injustice are not real</category id="3">
+                    <category id="3.1">3.1 - Questioning the science: There is not a scientific consensus that confirms the disproportionate impacts of environmental harm on marginalized communities.</category id="3.1">
+                    <category id="3.2">3.2 - Environment and justice don't mix: Social justice does not belong in environmental policies or movements.</category id="3.2">
+                    <category id="3.3">3.3 - Marginalized people are to blame for their own problems</category id="3.3">
+                    <category id="3.4">3.4 - Environmental justice is silly: Environmental justice ridiculous and even laughable, including sarcastic attitude to the EJ movement.</category id="3.4">
+                    <category id="4">4 - Environmental justice mischaracterization.</category id="4">
+                    <category id="4.1">4.1 - Environmental justice is racist: Environmental/climate justice movements/policies will only sew further division or otherwise lead to further marginalization.</category id="4.1">
+                    <category id="4.2">4.2 - Environmental justice has no clear definition</category id="4.2">
+                    <category id="4.3">4.3 - Environmental justice as trojan horse: Environmental/climate justice movements/policies are simply a way for politicians/activists to assert a malicious ulterior motive.</category id="4.3">
 
                     IMPORTANT:
                     - If none of the categories fit, please assign the category of '0.0'. 
